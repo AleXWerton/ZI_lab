1b4c54c33c85bbb31ed26b6f7ba3d1c4c9e14699
AleXWerton <chopalavov221@gmail.com>
2017-01-27 22:39:02 +0300

    Update AbstractValidator.java

--- carantine/src/secondModule/AbstractValidator.java
+++ carantine/src/secondModule/AbstractValidator.java
@@ -3,7 +3,7 @@ package secondModule;
 import data.Data;
 
 /**
- * Created by Руслан on 28.10.2016.
+ * Created by AleXWerton
  */
 public abstract class AbstractValidator {
     abstract boolean validate(Data data);
